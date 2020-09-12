9dba94bc0f07552aac363ce39d1b9ecd7cd6180b
muga-mark <markmuga01@gmail.com>
2020-09-13 02:50:44 +0800

    ReactPlayer: removed manual controls, added default controls

--- src/Chat/ChatBody.js
+++ src/Chat/ChatBody.js
@@ -1,18 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react';
 import ReactPlayer from 'react-player'
-import IconButton from '@material-ui/core/IconButton';
+// import IconButton from '@material-ui/core/IconButton';
 import NoEncryptionIcon from '@material-ui/icons/NoEncryption';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import StopIcon from '@material-ui/icons/Stop';
-import PauseIcon from '@material-ui/icons/Pause';
-import PictureInPictureAltIcon from '@material-ui/icons/PictureInPictureAlt';
-import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+// import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+// import StopIcon from '@material-ui/icons/Stop';
+// import PauseIcon from '@material-ui/icons/Pause';
+// import PictureInPictureAltIcon from '@material-ui/icons/PictureInPictureAlt';
+// import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import './ChatBody.css'
 
 function ChatBody({ messages, user, roomId }) {
     const messagesEndRef = useRef(null);
-    const [playing, setPlaying] = useState(false);
-    const [pip, setPip] = useState(false);
 
     const scrollToBottom = () => {
         if(roomId){
@@ -20,37 +18,6 @@ function ChatBody({ messages, user, roomId }) {
         }
     }
     useEffect(scrollToBottom, [messages]);
-
-    
-    const handlePlayPause = () => {
-        if(playing===false){
-            setPlaying(true)
-        }else{
-            setPlaying(false)
-        }
-    }
-
-    const handleStop = () => {
-        setPlaying(false)
-    }
-
-    const handleTogglePIP = () => {
-        if(pip===false){
-            setPip(true)
-        }else{
-            setPip(false)
-        }
-    }
-
-    const handleEnablePIP = () => {
-        console.log('onEnablePIP')
-        setPip(true);
-    }
-    
-    const handleDisablePIP = () => {
-        console.log('onDisablePIP')
-        setPip(false);
-    }
 
     return (
         <div>
@@ -72,22 +39,8 @@ function ChatBody({ messages, user, roomId }) {
                             <>
                             <ReactPlayer 
                                 url={message.video} 
-                                playing={playing}
-                                pip={pip}
-                                onEnablePIP={handleEnablePIP}
-                                onDisablePIP={handleDisablePIP}
+                                controls={true}
                             />
-                            <div className="chat__body_image_container_buttons">
-                                <IconButton onClick={handleStop}>
-                                    <StopIcon />
-                                </IconButton>
-                                <IconButton onClick={handlePlayPause}>
-                                    {playing ? <PauseIcon /> : <PlayArrowIcon />}
-                                </IconButton>
-                                <IconButton onClick={handleTogglePIP}>
-                                    {pip ? <OpenInNewIcon /> : <PictureInPictureAltIcon />}
-                                </IconButton>
-                            </div>
                             </>
                         : null}
                     </div>  
